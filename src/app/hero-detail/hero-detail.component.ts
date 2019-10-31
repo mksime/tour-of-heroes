a2a818dbd4b2c0253f717ba5f65003f38b6c4ed4
marcelosime <marcelo.sime@gmail.com>
2019-10-31 13:33:08 -0300

    completo

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../heroes';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,9 +15,30 @@ import { Hero } from '../heroes';
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location,
+    ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+// The route.snapshot is a static image of the route information shortly after the component was created.
+
+// The paramMap is a dictionary of route parameter values extracted from the URL. The "id" key returns the id of the hero to fetch.
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
   }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../heroes';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,20 +8,32 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
+  heroes: Hero[];
 
-  heroes = HEROES;
-  selectedHero: Hero;
+  constructor(private heroService: HeroService) { }
 
-  constructor() { }
-  
   ngOnInit() {
+    this.getHeroes();
   }
-  
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
+
+  // The new version waits for the Observable to emit the array of heroes—which could happen now or several minutes from now.
+
+  // The subscribe() method passes the emitted array to the callback, which sets the component's heroes property.heroes
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {return; }
+    this.heroService.addHero({name} as Hero).subscribe(hero => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter( h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
 }
